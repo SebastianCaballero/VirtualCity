706df1051106520d9a20f91a0228576510b1b171
SebastianCaballero <sebastian.caballero7@outlook.com>
2019-04-29 03:54:08 -0500

    Se creó la primera localidad, llamada NuevaAsgard (primer objeto de clase Localidad), el objeto puede ser modifcado y se observa el dinamismo que presenta con sus mñetodos. Ahora se procederá a crear más objetos como él y una clase que posea un algoritmo de comunicación

---
+++ principalCity.py
@@ -0,0 +1,10 @@
+import classLocalidad
+
+NuevaAsgard = classLocalidad.Localidad()
+NuevaAsgard.setHabitantes(200)
+NuevaAsgard.setEstrato(4)
+NuevaAsgard.setPresupuesto(2750)
+NuevaAsgard.setCiclovia(False)
+NuevaAsgard.setPublico(True)
+NuevaAsgard.setCarro(True)
+print(NuevaAsgard)
