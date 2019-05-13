5a922573c3cbac1d1001fa716bee222894420eda
SebastianCaballero <sebastian.caballero7@outlook.com>
2019-05-13 01:46:11 -0500

    Se mejoró la clase Localidad para que funcionara para una futura clase de relación entre localidades.

--- classLocalidad.py
+++ classLocalidad.py
@@ -7,28 +7,28 @@ MEJORAR GENERAR UN MECANISMO DE TRANSPORTE ÓPTIMO
 class Localidad:
 
     def __init__(self):
-        self.__habitantes = 120
-        self.__estrato = 3
-        self.__presupuesto = 1200
-        self.__ciclovia = False
-        self.__publico = False
-        self.__carro = False
+        self.habitantes = 120
+        self.estrato = 3
+        self.presupuesto = 1200
+        self.ciclovia = False
+        self.publico = False
+        self.carro = False
 
     def setHabitantes(self, habitantes=120):
         if 35 < habitantes < 500:
-            self.__habitantes = habitantes
+            self.habitantes = int(habitantes)
         else:
-            self.__habitantes = 120
+            self.habitantes = 120
     def getHabitantes(self):
-        return self.__habitantes
+        return self.habitantes
 
     def setEstrato(self, estrato=3):
         if 1 <= estrato <= 5:
-            self.__estrato = estrato
+            self.estrato = estrato
         else:
-            self.__estrato = 3
+            self.estrato = 3
     def getEstrato(self):
-        return self.__estrato
+        return self.estrato
 
     def setPresupuesto(self, presupuesto=1200):
         estrato = self.getEstrato()
@@ -39,78 +39,52 @@ class Localidad:
         presupuestoEstrato5 = 4800
         if estrato == 1:
             if presupuestoEstrato1-(presupuestoEstrato1/2) < presupuesto <= presupuestoEstrato1+(presupuestoEstrato1/2):
-                self.__presupuesto = presupuesto
+                self.presupuesto = presupuesto
             else:
-                self.__presupuesto = presupuestoEstrato1
+                self.presupuesto = presupuestoEstrato1
         elif estrato == 2:
             if presupuestoEstrato2-(presupuestoEstrato2/2) < presupuesto <= presupuestoEstrato2+(presupuestoEstrato2/2):
-                self.__presupuesto = presupuesto
+                self.presupuesto = presupuesto
             else:
-                self.__presupuesto = presupuestoEstrato2
+                self.presupuesto = presupuestoEstrato2
         elif estrato == 3:
             if presupuestoEstrato3-(presupuestoEstrato3/2) < presupuesto <= presupuestoEstrato3+(presupuestoEstrato3/2):
-                self.__presupuesto = presupuesto
+                self.presupuesto = presupuesto
             else:
-                self.__presupuesto = presupuestoEstrato3
+                self.presupuesto = presupuestoEstrato3
         elif estrato == 4:
             if presupuestoEstrato4-(presupuestoEstrato4/2) < presupuesto <= presupuestoEstrato4+(presupuestoEstrato4/2):
-                self.__presupuesto = presupuesto
+                self.presupuesto = presupuesto
             else:
-                self.__presupuesto = presupuestoEstrato4
+                self.presupuesto = presupuestoEstrato4
         elif estrato == 5:
             if presupuestoEstrato5-(presupuestoEstrato5/2) < presupuesto <= presupuestoEstrato5+(presupuestoEstrato5/2):
-                self.__presupuesto = presupuesto
+                self.presupuesto = presupuesto
             else:
-                self.__presupuesto = presupuestoEstrato5
+                self.presupuesto = presupuestoEstrato5
     def getPresupuesto(self):
-        return self.__presupuesto
+        return self.presupuesto
 
     def setCiclovia(self, ciclovia=False):
         if ciclovia == True or ciclovia == False:
-            self.__ciclovia = ciclovia
+            self.ciclovia = ciclovia
         else:
-            self.__ciclovia = False
+            self.ciclovia = False
     def getCiclovia(self):
-        return self.__ciclovia
+        return self.ciclovia
 
     def setPublico(self, publico=False):
         if publico == True or publico == False:
-            self.__publico = publico
+            self.publico = publico
         else:
-            self.__publico = False
+            self.publico = False
     def getPublico(self):
-        return self.__publico
+        return self.publico
 
     def setCarro(self, carro=False):
         if carro == True or carro == False:
-            self.__carro = carro
+            self.carro = carro
         else:
-            self.__carro = False
+            self.carro = False
     def getCarro(self):
-        return self.__carro
-
-    def __str__(self):
-        Ciclovia = None
-        TransportePublico = None
-        RutaVehicular = None
-        if self.getCiclovia() == True:
-            Ciclovia = 'si'
-        else:
-            Ciclovia = 'no'
-        if self.getPublico() == True:
-            TransportePublico = 'si'
-        else:
-            TransportePublico = 'no'
-        if self.getCarro() == True:
-            RutaVehicular = 'si'
-        else:
-            RutaVehicular = 'no'
-
-        infoHabitantes = 'La localidad posee {} habitantes.\n'.format(self.getHabitantes())
-        infoEstrato = 'La localidad tiene estrato {}.\n'.format(self.getEstrato())
-        infoPresupuesto = 'La localidad posee un presupuesto de {} cucupesos.\n'.format(self.getPresupuesto())
-        infoCiclovia = 'La localidad {} posee ciclovia.\n'.format(Ciclovia)
-        infoPublico = 'La localidad {} posee trasnporte publico.\n'.format(TransportePublico)
-        infoCarro = 'La localidad {} posee ruta vehicular.\n'.format(RutaVehicular)
-
-        return infoHabitantes + infoEstrato +  infoPresupuesto + infoCiclovia + infoPublico + infoCarro
+        return self.carro
