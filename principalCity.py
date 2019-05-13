7f56601f7d722f773707ca1d999ef01aa2c0f9b5
SebastianCaballero <sebastian.caballero7@outlook.com>
2019-05-13 02:03:55 -0500

    Se agregó en el archivo principal código testeando cinco instancias de Relacion para probar su funcionamiento correcto. Todo está bien pero se debe empezar a condicionar el comportamiento de la clase para que no haya incoherencias

--- principalCity.py
+++ principalCity.py
@@ -1,10 +1,97 @@
-import classLocalidad
+import relacion
 
-NuevaAsgard = classLocalidad.Localidad()
-NuevaAsgard.setHabitantes(200)
-NuevaAsgard.setEstrato(4)
-NuevaAsgard.setPresupuesto(2750)
-NuevaAsgard.setCiclovia(False)
-NuevaAsgard.setPublico(True)
-NuevaAsgard.setCarro(True)
-print(NuevaAsgard)
+#MONTERREY
+Monterrey = relacion.Relacion()
+Monterrey.setNombre()
+Monterrey.setHabitantes(350)
+Monterrey.setEstrato(4)
+Monterrey.setPresupuesto(2750)
+Monterrey.setCiclovia(False)
+Monterrey.setPublico(True)
+Monterrey.setCarro(True)
+
+Monterrey.setComparacionCiclovia()
+Monterrey.setComparacionPublico()
+Monterrey.setComparacionCarro()
+
+Monterrey.establecerMovimientosCiclovia()
+Monterrey.establecerMovimientosPublico()
+Monterrey.establecerMovimientosCarro()
+print(Monterrey)
+
+#COLINA
+Colina = relacion.Relacion()
+Colina.setNombre()
+Colina.setHabitantes()
+Colina.setEstrato()
+Colina.setPresupuesto()
+Colina.setCiclovia(True)
+Colina.setPublico(True)
+Colina.setCarro(True)
+
+Colina.setComparacionCiclovia()
+Colina.setComparacionPublico()
+Colina.setComparacionCarro()
+
+Colina.establecerMovimientosCiclovia()
+Colina.establecerMovimientosPublico()
+Colina.establecerMovimientosCarro()
+print(Colina)
+
+#TEOREMA7
+Teorema7 = relacion.Relacion()
+Teorema7.setNombre()
+Teorema7.setHabitantes(400)
+Teorema7.setEstrato(1)
+Teorema7.setPresupuesto(500)
+Teorema7.setCiclovia(True)
+Teorema7.setPublico(True)
+Teorema7.setCarro(False)
+
+Teorema7.setComparacionCiclovia()
+Teorema7.setComparacionPublico()
+Teorema7.setComparacionCarro()
+
+Teorema7.establecerMovimientosCiclovia()
+Teorema7.establecerMovimientosPublico()
+Teorema7.establecerMovimientosCarro()
+print(Teorema7)
+
+#LAVICTORIA
+LaVictoria = relacion.Relacion()
+LaVictoria.setNombre()
+LaVictoria.setHabitantes(480)
+LaVictoria.setEstrato(5)
+LaVictoria.setPresupuesto(4700)
+LaVictoria.setCiclovia(True)
+LaVictoria.setPublico(True)
+LaVictoria.setCarro(True)
+
+LaVictoria.setComparacionCiclovia()
+LaVictoria.setComparacionPublico()
+LaVictoria.setComparacionCarro()
+
+LaVictoria.establecerMovimientosCiclovia()
+LaVictoria.establecerMovimientosPublico()
+LaVictoria.establecerMovimientosCarro()
+print(LaVictoria)
+
+#JUGUETE
+Juguete = relacion.Relacion()
+Juguete.setNombre()
+Juguete.setHabitantes(390)
+Juguete.setEstrato(2)
+Juguete.setPresupuesto(750)
+Juguete.setCiclovia(True)
+Juguete.setPublico(True)
+Juguete.setCarro(True)
+
+Juguete.setComparacionCiclovia()
+Juguete.setComparacionPublico()
+Juguete.setComparacionCarro()
+
+Juguete.establecerMovimientosCiclovia()
+Juguete.establecerMovimientosPublico()
+Juguete.establecerMovimientosCarro()
+print(Juguete)
+
